ae64f42aeb95cce979efbeab52501ebe0223f040
横井一樹 <yokoikazuki@yokoikazuki.local>
2015-03-01 08:55:21 +0900

    Initial Commit

---
+++ MyTweet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyTweet
+//
+//  Created by 横井一樹 on 2015/03/01.
+//  Copyright (c) 2015年 LifeisTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyTweet/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MyTweet
+//
+//  Created by 横井一樹 on 2015/03/01.
+//  Copyright (c) 2015年 LifeisTech. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
